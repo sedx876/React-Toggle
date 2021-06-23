d84d1b442d91dc1a7fa3183c97b1cdf540951e71
sedx876 <swatkins876@gmail.com>
2021-06-23 09:19:56 -0400

    Added Toggle

---
+++ src/Toggle.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export default class Toggle extends Component {
+  state = {
+    on: false,
+  }
+
+  toggle = () => {
+    this.setState({
+      on: !this.state.on
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button class="btn-floating btn-large waves-effect waves-light black" onClick={this.toggle}>
+          <i class="fas fa-plus"
+            style={{color: 'red'}}></i></button>
+        {this.state.on && this.props.children}
+      </div>
+    )
+  }
+}
